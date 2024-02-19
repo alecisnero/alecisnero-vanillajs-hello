37757dc8df6a05f26e600bba41792d8b640d484a
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-02-19 19:33:10 +0000

    Feat/primer version verificada del generador de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  /* let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < ext.length; e++) {
+          let result = pronoun[p] + adj[a] + noun[n] + ext[e];
+          console.log(result);
+        }
+      }
+    }
+  } */
 };
